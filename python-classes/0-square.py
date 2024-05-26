56f24501e2c3b865fe13238b064ddf735f19f8c4
Raid-dev <a_rauf2004@mail.ru>
2024-05-26 18:11:44 +0000

    Added executable script

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module containing the definition of the Square class."""
 
+
 class Square:
     """A class representing a square."""
     pass
+
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module containing the definition of the Square class."""
 
+
 class Square:
     """A class representing a square."""
 
