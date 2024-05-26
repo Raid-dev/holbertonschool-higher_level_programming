405d5292501998216162bb7f4a3d5cc1f72ae57b
Raid-dev <a_rauf2004@mail.ru>
2024-05-26 13:44:10 +0000

    Added executable script

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print()
+        return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end=' ')
