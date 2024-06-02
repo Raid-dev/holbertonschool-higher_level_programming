73ab3b1ee5da72a5035ffbafa34796df85bf067e
Raid-dev <a_rauf2004@mail.ru>
2024-06-02 09:05:48 +0000

    Added module

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Integers Addition module"""
+
+def add_integer(a, b=98):
+    """ Adds two integers """
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+        return a + b
