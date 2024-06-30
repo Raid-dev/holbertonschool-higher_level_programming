87772dbf1d786fd3fea9d90d0cb84c1ed063d9f6
Raid-dev <a_rauf2004@mail.ru>
2024-06-30 19:17:51 +0000

    Added executable script

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Lists all states from the database hbtn_0e_0_usa.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id")
+    [print(state) for state in c.fetchall()]
+    c.close()
+    db.close()
