21300449eedde3d75465409c17c3f1c73775e735
Raid-dev <a_rauf2004@mail.ru>
2024-06-30 15:20:07 +0000

    Added executable script

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
--- Prints the following description of the table first_table from the database hbtn_0c_0
-
-USE hbtn_0c_0;
+-- Prints the following description of the table first_table from the database
 
 SHOW CREATE TABLE first_table;
 
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Lists all rows of the table first_table from the database
+
+SELECT * FROM first_table;
+
